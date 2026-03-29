0bb26036d98de191ea376c5206dc8f061e7f816a
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-03-29 15:44:15 +0530

    Update useSoundscape.ts

--- src/hooks/useSoundscape.ts
+++ src/hooks/useSoundscape.ts
@@ -5,7 +5,7 @@
 
 import { useEffect } from 'react';
 import { useEmotionStore } from '@/stores/emotionStore';
-import { transitionTo, destroySoundscape } from '@/core/audio/soundscapeEngine';
+import { transitionTo, destroySoundscape } from '@/core/audio/soundscapeEngine'; 
 
 export function useSoundscape() {
   const activeMood = useEmotionStore((s) => s.activeMood);
