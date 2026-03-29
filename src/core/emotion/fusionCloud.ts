20246b24d0711d750d7ea0d942f55e63a7f983bc
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-03-29 15:46:58 +0530

    Update fusionCloud.ts

--- src/core/emotion/fusionCloud.ts
+++ src/core/emotion/fusionCloud.ts
@@ -4,7 +4,7 @@ export async function fuseCloud(input: FusionInput): Promise<EmotionVector> {
   const res = await fetch("https://YOUR_CLOUD_FUSION_API/fuse", {
     method: "POST",
     body: JSON.stringify(input),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }, 
   });
 
   return await res.json();
